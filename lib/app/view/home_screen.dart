4ebbc58722abf04a528de404489d159d4a25202c
h-zaman <147a258s369dtaz>
2023-07-11 02:22:31 +0600

    home view in progress

--- lib/app/view/home_screen.dart
+++ lib/app/view/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:expensetracker/app/controller/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String route = '/HomeScreen';
@@ -9,14 +11,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final _controller = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Home Screen'
-        ),
+    return Obx(()=>Scaffold(
+      body: PageView(
+        onPageChanged: _controller.pageViewIndex,
+        controller: _controller.pageViewController,
+        children: _controller.homeViews
       ),
-    );
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _controller.pageViewIndex.value,
+        onTap: (index){
+          _controller.pageViewController.jumpToPage(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home
+            )
+          ),
+          BottomNavigationBarItem(
+            label: 'History',
+            icon: Icon(
+              Icons.history
+            )
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(
+              Icons.settings
+            )
+          ),
+        ],
+      ),
+    ));
   }
 }
---
+++ lib/app/view/settings_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeSettingsView extends StatelessWidget {
+  const HomeSettingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('HomeSettingsView'),);
+  }
+}
